3bbff2417a326015b8429a670d407525ad29295a
Olivier Xu <olivier.xu@gmail.com>
2014-01-06 18:10:30 +0100

    first solution to ruby-0 challenge 6

--- Wagon-0/06. Stupid coaching/stupid_coaching.rb
+++ Wagon-0/06. Stupid coaching/stupid_coaching.rb
@@ -1,5 +1,20 @@
-def stupid_coaching 
-  # your code goes here
+def stupid_coaching
+	puts "What do you want to tell me?"
+	answer = gets.chomp
+
+	until answer.downcase.include? "i am going to work"
+		puts "I can feel your motivation son!" if answer == answer.upcase
+		if answer.include? "?"
+			puts "Silly question, just get dressed and go to work!"
+		else
+			puts "I don't care son, just get dressed and go to work!"
+		end
+		puts "What else?"
+		answer = gets.chomp
+	end
+
+	puts "Good, hurry up son!"
+
 end
 
 # Calling the method
