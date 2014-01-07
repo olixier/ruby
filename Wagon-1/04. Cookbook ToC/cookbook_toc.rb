7b7ea3a16063415c3c342407e35629ec7b80595d
Olivier Xu <olivier.xu@gmail.com>
2014-01-07 16:02:52 +0100

    first attempt at ruby-1 ex4

--- Wagon-1/04. Cookbook ToC/cookbook_toc.rb
+++ Wagon-1/04. Cookbook ToC/cookbook_toc.rb
@@ -7,5 +7,12 @@ chapters = [
              ['Viandes et Gibiers', 38],
              ['Poissons, coquillages et crustaces', 157]
            ]
-           
-# You program goes here !
+
+# utiliser chapters.max { |x, y| (x + y).length } pour calculer width?
+
+puts title.center(70)
+puts ""
+chapters.each do |x, y|
+	print "Chapter #{chapters.index([x, y]) + 1} : #{x}".ljust(60)
+	puts "Page #{y}".rjust(10)
+end
