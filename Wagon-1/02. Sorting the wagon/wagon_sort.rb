4b5514b3976f2b06dbd322bac307718690e118a5
Olivier Xu <olivier.xu@gmail.com>
2014-01-07 12:42:46 +0100

    first solution to Ruby-1 ex2

--- Wagon-1/02. Sorting the wagon/wagon_sort.rb
+++ Wagon-1/02. Sorting the wagon/wagon_sort.rb
@@ -1,5 +1,17 @@
-def wagon_sort( students_array )
-  # your code to sort the array and output students
+def input_students (students_array)
+	while true
+		puts "New student?"
+		new_student = gets.chomp
+		break if new_student.empty?
+		students_array << new_student
+	end
 end
 
-# your code to interact with the user..
+def wagon_sort( students_array )
+	students_array.sort { |a, b| a.downcase <=> b.downcase }
+end
+
+students_array = Array.new
+input_students(students_array)
+puts "Congratulation! Your Wagon has #{students_array.count} students:"
+wagon_sort(students_array).each { |name| puts name }
