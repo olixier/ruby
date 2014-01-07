69d58d4ac807681440073c93c0c7510f2430f7de
Olivier Xu <olivier.xu@gmail.com>
2014-01-07 11:07:59 +0100

    first solution to ruby-1 ex1

--- Wagon-1/01. Simple looping/simple_looping.rb
+++ Wagon-1/01. Simple looping/simple_looping.rb
@@ -1,12 +1,35 @@
 # The objective is to create a function that computes the sum of the integers from a min value to a max value
 
-def sum(min,max)
-  # your code here
+def sum(min, max)
+	res = 0
+	for n in min..max
+		res += n
+	end
+	res
 end
+
+def sum2(min, max)
+	res = 0
+	n = min
+	while n <= max
+		res += n
+		n += 1
+	end
+	res
+end
+
+def sum3(min, max)
+	(max * (max + 1) - min * (min - 1)) / 2
+end
+
 
 # Testing your code
 
-min = 1
-max = 100
-sum = sum(1,100)
-puts sum == 5050 # => true
+puts "Min?"
+min = gets.to_i
+puts "Max?"
+max = gets.to_i
+
+puts "The 'for' method gives #{sum(min, max)}"
+puts "The 'while' method gives #{sum2(min, max)}"
+puts "The recursive method gives #{sum3(min, max)}"
