806744962d4a02b6043180861aac1f4ef76b5dae
Olivier Xu <olivier.xu@gmail.com>
2014-01-10 10:51:03 +0100

    first attempt at ruby-Z ex3

--- Wagon-2/03. Anagrams/anagrams.rb
+++ Wagon-2/03. Anagrams/anagrams.rb
@@ -1,13 +1,16 @@
 def anagrams?( a_string, another_string )
-  # your code goes here
+	a_string.downcase.chars.to_a.reject { |x| x == " " }.sort == another_string.downcase.chars.to_a.reject { |x| x == " " }.sort
 end
 
 def anagrams_on_steroids?( a_string, another_string )
-  # your code goes here
+	h = Hash.new(0)
+	a_string.downcase.chars.reject { |x| x == " " }.each { |letter| h[letter] += 1 }
+	another_string.downcase.chars.reject { |x| x == " " }.each { |letter| h[letter] -= 1 }
+	h.values.all? { |x| x == 0 }
 end
 
-# complexity = ?
+# complexity = O(n*log(n))
 puts anagrams? "Monica Lewinsky", "Nice silky woman" # => true 
 
-# complexity = ?
+# complexity = ? O(n)
 puts anagrams_on_steroids? "Monica Lewinsky", "Nice silky woman" # => true
