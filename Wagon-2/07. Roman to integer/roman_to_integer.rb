7d3be95b89e4d9c7c031af58e3166b134843f8d2
Olivier Xu <olivier.xu@gmail.com>
2014-01-10 18:19:15 +0100

    first attempt at ruby2 ex7

--- Wagon-2/07. Roman to integer/roman_to_integer.rb
+++ Wagon-2/07. Roman to integer/roman_to_integer.rb
@@ -1,9 +1,25 @@
+@EQUIV = { 'I'=>1, 'V'=>5, 'X'=>10, 'L'=>50, 'C'=>100, 'D'=>500, 'M'=>1000 }
+
+def scan(string, i, j)
+	return false if string[j].nil?
+	string[i..j].each_char.all? { |letter| letter == string[i] }
+end
+
 def roman_to_integer(roman_string)
-  # your code goes here
+	i, j, result = 0, 0, 0
+	while j < roman_string.length
+		if scan(roman_string, i, j)
+			j += 1
+		else
+			result += @EQUIV[roman_string[i]] < @EQUIV[roman_string[j]] ? @EQUIV[roman_string[j]] - @EQUIV[roman_string[i]] : @EQUIV[roman_string[i]] * (j - i)
+			i = j
+		end
+	end
+	result += @EQUIV[roman_string[i]] * (j - i) if roman_string[-1] == roman_string[-2]  # Degueu mais bon ca marche
+	return result
 end
 
 # testing your program
 puts roman_to_integer('IV') == 4 # => true
 puts roman_to_integer('XII') == 12 # => true
 puts roman_to_integer('XIX') == 19 # => true
-
