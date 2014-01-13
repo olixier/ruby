cb65e06b3ebc024457bbc11bb02a4b4987cba3f2
Olivier Xu <olivier.xu@gmail.com>
2014-01-13 11:33:21 +0100

    first attempt at ruby2 ex9

--- Wagon-2/09. Storing blocks in Proc/love_message.rb
+++ Wagon-2/09. Storing blocks in Proc/love_message.rb
@@ -1,25 +1,23 @@
 def tell(who, &message_block)
-  # A new Proc object is created from the given message_block 
-  # Your code goes here...
+	puts "#{who}, " + message_block.call
 end
 
 def tell_mum(&message_block)
-  #...and here (you should call the tell method of course)
+	puts "mum, " + message_block.call
 end
 
 tell_mum {"I love you"} # => "mum, I love you !"
 
 
 # Now create the block object by yourself
-def tell(who, message_blk)
-  # The Proc object is created outside the method and passed as normal parameter
-  # Your code goes here...
+def tell2(who, message_blk)
+	puts "#{who}, " + message_blk.call
 end
 
-def tell_mum(message_blk)
-  #...and here (you should call the tell method of course)
+def tell_mum2(message_blk)
+	puts "mum, " + message_blk.call
 end
 
 love_block = Proc.new {"I love you"}
 
-tell_mum(love_block) # => "mum, I love you !"
+tell_mum2(love_block) # => "mum, I love you !"
