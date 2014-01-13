d3606f65236db4f423ced7516efe10a5e9a07ac3
Olivier Xu <olivier.xu@gmail.com>
2014-01-13 21:59:58 +0100

    recursive answer to ruby2 ex1

--- Wagon-2/01. Modeling with Hash/calories.rb
+++ Wagon-2/01. Modeling with Hash/calories.rb
@@ -9,6 +9,38 @@ def calories_in2(burger, side, beverage)
 	food[burger] + food[side] + food[beverage]
 end
 
+def calories_in3(*meal)
+	meal = meal[0] if meal[0].is_a?(Array) and meal.length == 1 # This is in case we have an array of array with the recursion
+
+	conversion_hash = {
+		"Cheese Burger" => 290, 
+		"Big Mac" => 300, 
+		"Mc Bacon" => 400, 
+		"Royal Cheese" => 130, 
+		"French fries" => 130, 
+		"Potatoes" => 130, 
+		"Coca" => 160, 
+		"Filet-of-Fish" => 300,
+		"Happy Meal" => ["Cheese Burger", "French fries", "Coca"]
+	}
+
+		calories = 0
+
+		meal.each do |ingredient|
+			value = conversion_hash(ingredient)
+			if value.is_a?(Array)
+				calories += calories_in3(value) # recursive method if values is an array = combo of ingredients
+			else
+				calories += value
+			end
+		end
+
+		return calories
+
+	end
+
+
+
 puts "Burger?"
 burger = gets.chomp
 puts "Side?"
@@ -18,6 +50,10 @@ beverage = gets.chomp
 
 puts calories_in(burger, side, beverage)
 puts calories_in2(burger, side, beverage)
+
+puts "And the calories for the Happy Meal is"
+puts calories_in3("Happy Meal")
+
 puts "Congrats, you're fat!"
 
 # if we need hashes to define meals
